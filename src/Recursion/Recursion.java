89cb3bdffee1fefcf7920a1c502d686acacaa7a4
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2024-07-14 20:31:12 +0700

    Fibonacci 2

--- src/Recursion/Recursion.java
+++ src/Recursion/Recursion.java
@@ -3,9 +3,18 @@ package Recursion;
 public class Recursion {
     public static void main(String[] args) {
         System.out.println(fact(25));
-        System.out.println(fib(12));
+        System.out.println(fib(19));
+        System.out.println(fibonacci(19));
     }
 
+    //Fibonacci but shorter
+    public static int fibonacci(int n) {
+        if (n >= 2)
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        else return n;
+    }
+
+    //Fibonacci
     public static int fib(int n) {
         if (n == 0)
             return 0;
