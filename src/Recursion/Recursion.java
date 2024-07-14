caf24c0da7b1a3b6e8661070d59c3cf6bb4925f4
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2024-07-14 20:03:19 +0700

    fibonacci

--- src/Recursion/Recursion.java
+++ src/Recursion/Recursion.java
@@ -3,6 +3,17 @@ package Recursion;
 public class Recursion {
     public static void main(String[] args) {
         System.out.println(fact(25));
+        System.out.println(fib(12));
+    }
+
+    public static int fib(int n) {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else if (n >= 2)
+            return fib(n - 1) + fib(n - 2);
+        else return -1;
     }
 
     //Factorial
