ca03e8c4bbf1408912cf3cc3886cab9ec19b8590
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2024-07-31 10:55:29 +0700

    88. Remove duplicates from LinkedList

--- src/RemoveDuplicatesFromSortedLL_83/Solution.java
+++ src/RemoveDuplicatesFromSortedLL_83/Solution.java
@@ -5,17 +5,37 @@ public class Solution {
         ListNode head = new ListNode(1, null);
         ListNode head2 = new ListNode(1, null);
         ListNode head3 = new ListNode(2, null);
+        ListNode head4 = new ListNode(3, null);
+        ListNode head5 = new ListNode(3, null);
 
         head.next = head2;
         head2.next = head3;
+        head3.next = head4;
+        head4.next = head5;
 
         ListNode result = new ListNode();
         result = deleteDuplicates(head);
         show(result);
     }
 
+    //Maybe we can use 2 pointers technique
     public static ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;
+        ListNode left = new ListNode();
+        ListNode right = new ListNode();
+        left = head;
+        right = left.next;
 
+        while (left.next != null) {
+            if (left.val == right.val) {
+                left.next = right.next;
+            } else {
+                left = left.next;
+            }
+            right = right.next;
+        }
+
+        return head;
     }
 
     public static class ListNode {
