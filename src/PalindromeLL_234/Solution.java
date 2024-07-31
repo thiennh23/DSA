3936e6b13fa99d6d61d23fb60ba55f7ddb546e0b
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2024-07-31 12:03:04 +0700

    Palindrome but stuck at last testcase

--- src/PalindromeLL_234/Solution.java
+++ src/PalindromeLL_234/Solution.java
@@ -1,27 +1,43 @@
 package PalindromeLL_234;
 
-import LinkedList.LinkedL;
-
 public class Solution {
 
     public static void main(String[] args) {
 
         //Test case 1
-        ListNode testcase1 = new ListNode(1, null);
+        ListNode head = new ListNode(1, null);
         ListNode a = new ListNode(2, null);
         ListNode b = new ListNode(2, null);
         ListNode c = new ListNode(1, null);
 
-        testcase1.next = a;
+        head.next = a;
         a.next = b;
         b.next = c;
 
-        System.out.println(isPalindrome(testcase1));
+        /*int[] values = {1, 2, 2, 1, 5, 7, 5, 3, 8, 9, 0, 6, 5, 9, 6, 9, 2, 1, 3, 0, 8, 8, 4, 8, 9, 0, 3, 5, 6, 8, 9, 0, 1, 3, 2, 1, 0, 9, 8, 6, 5, 3, 0, 9, 8, 4, 8, 8, 0, 3, 1, 2, 9, 6, 9, 5, 6, 0, 9, 8, 3, 5, 7, 5, 1, 2, 2, 1};
+        ListNode head = createLinkedList(values);*/
+
+        System.out.println(isPalindrome(head));
+    }
+
+    // Helper method to create a linked list from an array
+    public static ListNode createLinkedList(int[] values) {
+        if (values == null || values.length == 0) return null;
+
+        ListNode head = new ListNode(values[0]);
+        ListNode current = head;
+
+        for (int i = 1; i < values.length; i++) {
+            current.next = new ListNode(values[i]);
+            current = current.next;
+        }
+
+        return head;
     }
 
     //This solution works almost every testcase
-    //But the last test case is so so so so big, so i think i will use string for this =))
-    public static boolean isPalindrome(ListNode head) {
+    //But the last test case is so so so so big. So i need to think about another way
+    /*public static boolean isPalindrome(ListNode head) {
         boolean result = false;
 
         long normalNum = 0;
@@ -39,6 +55,25 @@ public class Solution {
             return true;
 
         return result;
+    }*/
+
+    //Use this String technique instead
+    public static boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast!= null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        //Slow also is the middle of the linkedlist
+        //Now we will reverse the linkedlist
+        ListNode reverse = new ListNode();
+        while (slow != null) {
+
+        }
+
     }
 
     public static class ListNode {
